dbfb50fca00152424ee7435bd6da70eeab23cf8d
Pritam <pritamsahaofficial95@gmail.com>
2026-01-13 23:50:43 +0530

    added another section in fibo code and updated readme

--- basic-js-problems/1-Fibonacci.js
+++ basic-js-problems/1-Fibonacci.js
@@ -15,3 +15,24 @@ function printFibonacciSeries(n){
 }
 
 console.log(printFibonacciSeries(5));
+
+function nthFibo(n) {
+    //to print the nth fibonacci number
+    if (n === 0 || n === 1){
+        return n;
+    }
+    else {
+        return nthFibo(n-1)+nthFibo(n-2);
+    }
+};
+
+// callback
+function getNthFiboAns(n, fiboFn){
+    console.log(`The ${n}-th fibonacci number: ${fiboFn(n)}`);
+}
+getNthFiboAns(0, nthFibo)
+getNthFiboAns(1, nthFibo)
+getNthFiboAns(2, nthFibo)
+getNthFiboAns(3, nthFibo)
+getNthFiboAns(4, nthFibo)
+
--- basic-js-problems/README.md
+++ basic-js-problems/README.md
@@ -3,7 +3,7 @@
 
 ## Questions
 1. Print the Fibonacci Series. [Solution](./1-Fibonacci.js)
-2. Round off a Number to Next Multiple of 5.
-3. Find the Largest 3 Numbers
+2. Round off a Number to Next Multiple of 5. [Solution](./2-roundNumToNext5x.js)
+3. Find the Largest 3 Numbers. [Solution](./3-findLargest3Nums.js)
    - Input: findLargest3Numbers([45, 23, 78, 12, 90, 67, 34])
    - Output: [90, 78, 67]
