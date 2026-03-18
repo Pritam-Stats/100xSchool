a51c2d17c298d01c830394ae737ef34d9d147899
Pritam <pritamsahaofficial95@gmail.com>
2026-03-18 17:06:05 +0530

    Basic sign up sign in without jwt

--- 100xDevs/learning-codes-notes/w9-notes-app-backend/app.js
+++ 100xDevs/learning-codes-notes/w9-notes-app-backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express()
+const jwt = require("jsonwebtoken")
 
 // home page
 const path = require("path")
@@ -29,6 +30,74 @@ app.get("/getnotes", (req, res) => {
         notes
     }))
 })
+
+
+let users = []  //array of objects {username: username, password: password}
+
+app.get("/signuppage", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/signup.html"))
+})
+
+
+app.post("/signup", (req, res) => {
+    // const username = req.body.username;
+    // const password = req.body.password;
+    // const name = req.body.name
+
+    const { name, username, password } = req.body   //shorthand of the above
+
+    //check username
+    const usernameExist = users.find(user => user.username === username);
+
+    if (usernameExist) {
+        return res.status(409).json({
+            message: "User with this username already exists, enter an unique username"
+        })
+    };
+
+    //if it passed add to the users
+    users.push({
+        name: name,
+        username: username,
+        password: password
+    });
+
+    res.json({
+        message: "Account created; You can signin now"
+    })
+});
+
+app.get("/signinpage", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/signin.html"))
+})
+
+
+//signin
+app.post("/signin", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check credentials
+    const verified = users.find(user => user.username === username && user.password === password);
+
+    if (!verified) {
+        return res.status(403).json({
+            message: "wrong credentials"
+        })
+    };
+
+    const name = 
+    res.json({
+        message : "you are in ",
+        name: verified.name
+    })
+
+
+})
+
+
+
+
 
 app.listen(3000, ()=>{
     console.log("Server running on 3000")
