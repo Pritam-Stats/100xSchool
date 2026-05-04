f2af07d73d087b4abd191da4761cce1a4dfd95e6
Pritam <pritamsahaofficial95@gmail.com>
2026-05-04 23:38:31 +0530

    ts practice

--- 100xDevs/Learning-TypeScript/index.ts
+++ 100xDevs/Learning-TypeScript/index.ts
@@ -1,19 +1,47 @@
-import figlet from "figlet";
-import index from './index.html';
+// Hello world
 
-console.log("Hello via Bun!");
+function greet(name: string) {
+  console.log(`Hey, ${name}`);
+}
+
+greet("Pritam");
+
+// sum fn return ans
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(2, 3));
+
+// Type Interface
+interface User {
+  name: string;
+  age: number;
+  id?: null | number; // ? makes the id optional
+}
+
+function isLegal(user: User):string {
+  return Math.floor(user["age"]) >= 18
+    ? `${user["name"]} is allowed`
+    : `${user["name"]} is Not Allowed`;
+}
 
 
-const server = Bun.serve({
-  port: 3000,
-  routes: {
-    "/": () => new Response("Bun!"),
-    "/index" : index,
-    "/figlet" : () => {
-      const body = figlet.textSync('Bun');
-      return new Response(body);
-    }
-  }
-});
+const user1: User = {
+  name: 'Pritam',
+  age: 18.5,
 
-console.log(`Listening on ${server.url}`);
+} 
+
+console.log(isLegal(user1));
+
+
+//delayed call
+
+// giving func as an argument
+function delayedCall(fn: () => void, delay: number) : void {
+  setTimeout(fn, delay)
+}
+
+delayedCall(() => {console.log("Hi there")}, 1000);
